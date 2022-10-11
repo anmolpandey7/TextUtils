bc07671d72fc7a42026211caa5dfe03216946dce
Anmol Pandey <108005069+anmolpandey7@users.noreply.github.com>
2022-10-11 19:50:03 +0530

    Add files via upload

---
+++ TextUtils/TextUtils/README.md
@@ -0,0 +1,2 @@
+# TextUtils
+This is an Text Utillity app. it will help to analyze youe text in way you want.
---
+++ TextUtils/precache-manifest.1b8a561b3410642bf7aa62a8e22aec26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "606de89ede24c49cd356",
+    "url": "/static/css/main.2bd5beab.chunk.css"
+  },
+  {
+    "revision": "606de89ede24c49cd356",
+    "url": "/static/js/main.606de89e.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "299faf9390feffb5943c",
+    "url": "/static/js/runtime~main.299faf93.js"
+  },
+  {
+    "revision": "4efa5b46da4a90a4434af94870fba35d",
+    "url": "/index.html"
+  }
+];
